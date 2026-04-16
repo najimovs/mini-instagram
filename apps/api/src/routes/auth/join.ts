09260f2dc2afcc5f98f35c749bf104fc07b0bb5f
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-16 20:21:21 +0500

    feat(api): add mock signup

--- apps/api/src/routes/auth/join.ts
+++ apps/api/src/routes/auth/join.ts
@@ -1,3 +1,5 @@
+import { users } from "./db.ts"
+
 const bodyJSONSchema = {
 	type: "object",
 	required: [
@@ -22,15 +24,44 @@ export const schema = {
 	body: bodyJSONSchema,
 }
 
-export function route( req ) {
+export function route( req, res ) {
 
-	const { email, username, password } = req.body
+	let { email, username, password } = req.body
 
-	console.log( email )
-	console.log( username )
-	console.log( password )
+	username = username.toLowerCase()
+	email = email.toLowerCase()
+
+	const usernamePattern = /^(?=.{5,40}$)[a-z]+(_[a-z]+)*(_[0-9]+|[0-9]*)$/
+	const emailPattern = /^(?=.{1,254}$)[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]{1,255}\.[a-zA-Z]{2,}$/
+	const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]).{8,64}$/
+
+	if ( users.has( username ) ) {
+
+		return res.status( 400 ).send( { code: "API_AUTH_USERNAME_EXISTS" } )
+	}
+	else if ( !usernamePattern.test( username ) ) {
+
+		return res.status( 400 ).send( { code: "API_AUTH_USERNAME_INVALID" } )
+	}
+
+	if ( !emailPattern.test( email ) ) {
+
+		return res.status( 400 ).send( { code: "API_AUTH_EMAIL_INVALID" } )
+	}
+
+	if ( !passwordPattern.test( password ) ) {
+
+		return res.status( 400 ).send( { code: "API_AUTH_PASSWORD_INVALID" } )
+	}
+
+	//
+
+	users.set( username, {
+		email,
+		password,
+	} )
 
 	return {
-		login: true,
+		code: "API_AUTH_OK",
 	}
 }
