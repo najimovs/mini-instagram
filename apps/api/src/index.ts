4ecdbbdea1bca768b388cfabbe18ec671349f720
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-04-16 19:01:54 +0500

    feat(api): setup Fastify server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,27 @@
-console.log( "API" )
+import Fastify from "fastify"
+
+const fastify = Fastify( {
+	logger: true,
+} )
+
+const PORT = parseInt( process.env.VITE_API_PORT ) || 3_100
+
+fastify.get( "/", () => {
+
+	return {
+		message: "OK",
+	}
+} )
+
+try {
+
+	await fastify.listen( { port: PORT } )
+
+	fastify.log.info( `API ready at: ${ PORT }` )
+
+} catch ( err ) {
+
+	fastify.log.error( err )
+
+	process.exit( 1 )
+}
