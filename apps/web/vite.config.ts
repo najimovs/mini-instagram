273f0fea6a9c94c82f9c6d0441add397426ffa2b
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-05-05 19:13:30 +0500

    feat: load env

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite"
+import path from "path"
+import { defineConfig, loadEnv } from "vite"
 import react, { reactCompilerPreset } from "@vitejs/plugin-react"
 import babel from "@rolldown/plugin-babel"
 
-export default defineConfig( {
-	server: {
-		host: true,
-		port: 5100,
-	},
-	plugins: [
-		react(),
-		babel( { presets: [ reactCompilerPreset() ] } ),
-	],
+export default defineConfig( ( { mode } ) => {
+
+	const env = loadEnv( mode, path.resolve( __dirname, "../../" ), "" )
+	const port = parseInt( env.VITE_WEB_PORT )
+
+	return {
+		server: {
+			host: true,
+			port: port,
+		},
+		plugins: [
+			react(),
+			babel( { presets: [ reactCompilerPreset() ] } ),
+		],
+	}
 } )
